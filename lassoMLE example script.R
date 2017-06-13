e4278481fc9f83244a3029b6dc8ce9e9e4460761
amitmeir <amitmeir@uw.edu>
2017-06-13 15:45:22 -0700

    Update to lasso simulation script

--- lassoMLE example script.R
+++ lassoMLE example script.R
@@ -29,22 +29,16 @@ generate.regression.data <- function(n,sqrtSigma,numberNonzero,snr=2,ysig=1) {
 }
 
 # Parameters ----------------
-set.seed(140) #!!
-set.seed(143)
-#set.seed(135)
-# n <- 300
-# p <- 200
-n <- 200
+n <- 400
 p <- 400
-snr <- 0.2
+snrEta <- 0.5
 numberNonzero <- 4
 rho <- 0.5
 
 # Generating Data ----------------------
 Xsqrtsig <- generate.sqrt.Sigma(p, rho, sigsq = 1)$sqrt
 s <- 0
-while(s < 2 | s > 40) {
-  snrEta <- snr
+while(s < 2 | s > n / 4) {
   regData <- generate.regression.data(n, Xsqrtsig, numberNonzero, snr = snr, ysig = 1)
   X <- regData$X[1:n, ]
   X <- apply(X, 2, function(x) (x - mean(x))/sd(x))
@@ -83,11 +77,10 @@ selectiveFit <- fixedLassoInf(X, y, alpha = 0.05,
                               sigma = selectiveysig)
 
 # Conditional MLE ----------------
-assumeConvergence <- 1400
 mle <- lassoMLE(y, X, lambda = "lambda.min",
                  ysig = NULL, lassoFit = lassoFit,
-                 delay = 20, assumeConvergence = assumeConvergence,
-                 iterations = 2000, stepRate = 0.8)
+                 delay = 20, optimSteps = 1000,
+                 sampSteps = 3000, stepRate = 0.8, method = "selected")
 
 conditional <- mle$conditionalBeta
 # Solution Path -------------------------
@@ -117,15 +110,14 @@ naiveSD <- summary(naiveFit)$coefficients[, 2]
 naiveCI <- cbind(naiveBeta - 1.96 * naiveSD, naiveBeta + 1.96 * naiveSD)
 
 # CI and estimate example --------------------
-lassoBeta <- as.vector(coef(lassoFit, s = lambda / n))[-1]
-lassoBeta <- lassoBeta[lassoBeta != 0]
-offset <- 0.15
+lassoBeta <- as.numeric(coef(lassoFit))[-1][selected]
+offset <- 0.2
 naivedat <- data.frame(estimate = naiveBeta, variable = rank(naiveBeta),
                        lCI = naiveCI[, 1], uCI = naiveCI[, 2],
                        method = "naive", offset = offset)
 conddat <- data.frame(estimate = conditional, variable = rank(naiveBeta),
                       lCI = conditionalCI[, 1], uCI = conditionalCI[, 2],
-                      method = "conditional", offset = 0)
+                      method = "mle", offset = 0)
 lassodat <- data.frame(estimate = lassoBeta, variable = rank(naiveBeta),
                       lCI = selectiveCI[, 1], uCI = selectiveCI[, 2],
                       method = "lasso", offset = offset * 2)
@@ -133,24 +125,15 @@ truedat <- data.frame(estimate = true, variable = rank(naiveBeta),
                        lCI = NA, uCI = NA,
                        method = "true", offset = offset * 3)
 forplot <- rbind(naivedat, lassodat, conddat, truedat)
-forplot$method <- factor(as.character(forplot$method), levels = c("conditional", "naive", "true", "lasso"))
+forplot$method <- factor(forplot$method, levels = c("mle", "naive", "lasso", "true"))
 
-forci <- forplot
-forci <- subset(forplot, !(method %in% c("laasso", "true")))
-forci$method <- as.character(forci$method)
-forci$method[forci$method == "lasso"] <- "polyhedral"
-forci$method[forci$method == "conditional"] <- "conditional-wald"
-forci$method <- factor(forci$method, levels = c("conditional-wald", "naive", "polyhedral"))
-slack <- 0.01
+slack <- 0.1
 cimax <- max(conditionalCI) + slack
 cimin <- min(conditionalCI) - slack
-
-# pdf("lassoEstimatesCI.pdf",pagecentre=T, width=8,height=3.5 ,paper = "special")
-ggplot(subset(forplot)) +
+ggplot(forplot) +
   geom_point(aes(x = variable + offset, y = estimate, shape = method)) +
-  geom_segment(data = forci, aes(x = variable + offset, xend = variable + offset,
+  geom_segment(aes(x = variable + offset, xend = variable + offset,
                    y = pmax(lCI, cimin), yend = pmin(uCI, cimax), col = method, linetype = method)) +
   theme_bw() + geom_hline(yintercept = 0) + xlab("Variable") +
   ylab("Estimates/CIs")
-# dev.off()
 
